02b9e5a4f8e73eb1bfa406420a448c5c4a4b2a18
minde bal <mindebaltru@gmail.com>
2020-01-28 14:51:10 +0100

    pakoreguotas veikiantis kodas

--- Sum.php
+++ Sum.php
@@ -21,19 +21,18 @@ class Sum {
 	 	}
 	 	$liekana = $sum % 11;
 	 	echo '11 skaicius - '. $liekana. PHP_EOL;
+	 	$step1 = 0;
+	 	$step2 = 0;
 	 	//S = A*3 + B*4 + C*5 + D*6 + E*7 + F*8 + G*9 + H*1 + I*2 + J*3
-	 	for ($i=0; $i<strlen($kodas); $i++){
+	 	for ($i=0; $i<strlen($kodas)-1; $i++){
 	 		if ($i < 7){
-	 			$sum1 += $kodas[$i] * ($i +3);
-	 			echo $kodas[$i] . " ($i +1)" . ($i +1) . " " . ($kodas[$i] * ($i +1)) . " " . $sum1 . PHP_EOL;
+	 		    $step1 = ($i +3);
+	 			$sum1 += $kodas[$i] * $step1;
+	 			echo $kodas[$i] . " ($i +3)" . $step1 . " " . ($kodas[$i] * $step1) . " " . $sum1 . PHP_EOL;
 
-	 		} 
-	 	}
-	 	for ($i=0; $i<strlen($kodas); $i++){
-	 		if (($i > 6) && ($i < 10)) {
-	 			$sum2 += $kodas[$i] * $i;
-	 		    echo $kodas[$i] . "($i +1)" . ($i +1) . " " . ($kodas[$i] * ($i +1)) . " " . $sum2 . PHP_EOL;
-	 		    	
+	 		} elseif (($i >= 7)) {
+	 		    $step2++;
+	 			$sum2 += $kodas[$i] * $step2;
 	 		}
 	 	}
 	 	$sum3 = $sum2 + $sum1;
@@ -42,3 +41,4 @@ class Sum {
 	 	echo '11 skaicius - '. $liekana1. PHP_EOL;
 	 }
 }
+$obj = new Sum('33309240064');
